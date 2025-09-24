e76ed82d25f3a7b8aff9707fa59aab01a87e1f07
manafalb <tuv00310@temple.edu>
2025-09-24 13:32:39 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,26 @@
-let count = 0
+let counter = 0
+let time = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (input.lightLevel() / 30))
+})
+input.onButtonPressed(Button.AB, function () {
+    counter = 0
+    time = 0
+})
 loops.everyInterval(1000, function () {
-    if (input.lightLevel() > 25) {
-        count += 1
-    } else if (false) {
-    	
-    } else {
-    	
+    time += 1
+    if (counter >= 10) {
+        basic.showIcon(IconNames.No)
+    } else if (input.lightLevel() < 25) {
+        basic.showIcon(IconNames.Sad)
+        counter += 1
+    } else if (input.lightLevel() >= 25) {
+        basic.showIcon(IconNames.Yes)
     }
 })
 loops.everyInterval(2000, function () {
     serial.writeValue("light level", input.lightLevel())
+    if (time >= 60) {
+        game.pause()
+    }
 })
